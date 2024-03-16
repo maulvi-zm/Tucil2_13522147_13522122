0a73d869430e07f2ec17723585beeec252a8c2d5
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-03-16 11:33:26 +0700

    feat: add comment

--- src/src/utils/dnc.ts
+++ src/src/utils/dnc.ts
@@ -1,5 +1,12 @@
 import { BezierResult, Point } from "./data-structure";
 
+/**
+ * Finds the midpoint between two points.
+ *
+ * @param p1 - The first point.
+ * @param p2 - The second point.
+ * @returns The midpoint between p1 and p2.
+ */
 function findMidPoint(p1: Point, p2: Point): Point {
   return {
     x: (p1.x + p2.x) / 2,
@@ -7,6 +14,14 @@ function findMidPoint(p1: Point, p2: Point): Point {
   };
 }
 
+/**
+ * Generates a new set of control points and points for a curve based on the given points and control points.
+ *
+ * @param points - An array of points representing the curve.
+ * @param controlPoints - An array of control points for the curve.
+ * @returns An object containing the new control points and points for the curve.
+ * @throws Error if the number of control points is invalid.
+ */
 function makeCurve(
   points: Point[],
   controlPoints: Point[]
@@ -14,6 +29,7 @@ function makeCurve(
   controlPoints: Point[];
   points: Point[];
 } {
+  // Check if the number of control points is valid, just in case
   if (points.length !== controlPoints.length + 1) {
     throw new Error("Invalid control points");
   }
@@ -25,25 +41,31 @@ function makeCurve(
   let newPoints: Point[] = [];
 
   while (resultPointer < points.length - 1) {
+    // Check for current result pointer and control pointer
     const point1 = points[resultPointer];
     const point2 = points[resultPointer + 1];
     const controlPoint = controlPoints[controlPointer];
 
+    // Calculate the new control points
     const q0: Point = findMidPoint(point1, controlPoint);
     const q1: Point = findMidPoint(controlPoint, point2);
 
+    // Calculate the new points
     const s1: Point = findMidPoint(q0, q1);
 
+    // Push the new control points and points to their respective arrays
     newControlPoints.push(q0);
     newControlPoints.push(q1);
 
     newPoints.push(point1);
     newPoints.push(s1);
 
+    // Increment the pointers
     resultPointer += 1;
     controlPointer += 1;
   }
 
+  // Push the last point to the new points array
   newPoints.push(points[points.length - 1]);
 
   return {
@@ -52,17 +74,26 @@ function makeCurve(
   };
 }
 
+/**
+ * Calculates the Bezier curves based on the given points and iteration.
+ * @param points - The array of points to create the Bezier curves.
+ * @param iteration - The number of iterations to perform for creating the Bezier curves.
+ * @returns An object containing the matrix of points representing the Bezier curves and the time taken to calculate them.
+ */
 export function bezierCurves(points: Point[], iteration: number): BezierResult {
+  // If there's only one point, return it immediately
   if (points.length === 1) {
     return { matrix: [points], time: 0 };
   }
 
+  //Make container for control points and main points
   let controlPoints: Point[] = [];
   let mainPoints: Point[] = [];
   let result: Point[][] = [];
 
   let timeStart = performance.now();
 
+  // If the number of points is odd, then the odd point is a main point
   if (points.length % 2 == 1) {
     for (let i = 0; i < points.length; i += 1) {
       if (i % 2 == 0) {
@@ -72,13 +103,18 @@ export function bezierCurves(points: Point[], iteration: number): BezierResult {
       }
     }
   } else {
+    // If the number of points is even, then do the first iteration
+
+    // Calculate the control points
     for (let i = 0; i < points.length - 1; i += 1) {
       let midPoint: Point = findMidPoint(points[i], points[i + 1]);
       controlPoints.push(midPoint);
     }
 
+    // Push the first point to the main points
     mainPoints.push(points[0]);
 
+    // Calculate the main points based on the control points
     for (let i = 0; i < controlPoints.length - 1; i += 1) {
       let midPoint: Point = findMidPoint(
         controlPoints[i],
@@ -87,12 +123,15 @@ export function bezierCurves(points: Point[], iteration: number): BezierResult {
       mainPoints.push(midPoint);
     }
 
+    // Push the last point to the main points
     mainPoints.push(points[points.length - 1]);
 
+    // Push the first iteration to the result
     result.push(mainPoints);
     iteration -= 1;
   }
 
+  // Perform the iterations
   for (let i = 0; i < iteration; i++) {
     const newCurve = makeCurve(mainPoints, controlPoints);
     controlPoints = newCurve.controlPoints;
