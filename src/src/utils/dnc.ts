a2af325efedd29b053094417b57b85733aea9017
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-03-19 00:05:57 +0700

    feat: faster dnc

--- src/src/utils/dnc.ts
+++ src/src/utils/dnc.ts
@@ -18,46 +18,26 @@ function newControlPoint(points: Point[]): Point[] {
   if (points.length <= 1) {
     return points;
   }
-  let midPoints: Point[] = [];
-
+  const midPoints: Point[] = [];
   for (let i = 0; i < points.length - 1; i++) {
     midPoints.push(findMidPoint(points[i], points[i + 1]));
   }
-
-  return [points[0]]
-    .concat(newControlPoint(midPoints))
-    .concat(points[points.length - 1]);
+  return [points[0], ...newControlPoint(midPoints), points[points.length - 1]];
 }
 
 function DivideAndConquer(points: Point[], iteration: number): Point[] {
   let temp = newControlPoint(points);
-
   if (iteration <= 1) {
     return temp;
   }
-
-  let leftSlice = temp.slice(0, points.length);
-  let rightSlice = temp.slice(points.length - 1, temp.length);
-
+  const leftSlice = temp.slice(0, points.length);
+  const rightSlice = temp.slice(points.length - 1, temp.length);
   const left = DivideAndConquer(leftSlice, iteration - 1);
   const right = DivideAndConquer(rightSlice, iteration - 1);
-
   return left.concat(right.slice(1));
 }
 
 export function bezierCurves(points: Point[], iterations: number): Point[] {
-  let result: Point[] = [];
-  result = DivideAndConquer(points, iterations);
-
-  result = result.filter((_, index) => index % (points.length - 1) === 0);
-
-  return result;
+  let result: Point[] = DivideAndConquer(points, iterations);
+  return result.filter((_, index) => index % (points.length - 1) === 0);
 }
-
-const test: Point[] = [
-  { x: 0, y: 0 },
-  { x: 2, y: 4 },
-  { x: 4, y: 0 },
-];
-
-console.log(bezierCurves(test, 4).length);
